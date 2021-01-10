15f850bfc306a645a822df3a01edc2ee2783f125
Dongkyun Lee <dongkyl@amazon.com>
2021-01-10 11:52:54 +0900

    modify config

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,10 +2,10 @@
 import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
 import { InfraStack } from '../lib/infra-stack'
-import { App, Pipeline } from '../lib/interfaces/config'
+import { App, Repository } from '../lib/interfaces/config'
 
 const app = new cdk.App()
 new InfraStack(app, `${App.ns}InfraStack`, {
-  repositoryName: Pipeline.Repository.Name,
-  repositoryRegion: Pipeline.Repository.Region,
+  repositoryName: Repository.Name,
+  repositoryRegion: Repository.Region,
 })
--- infra/lib/interfaces/config.ts
+++ infra/lib/interfaces/config.ts
@@ -2,9 +2,7 @@ export namespace App {
   export const ns = 'S3DeployDemo'
 }
 
-export namespace Pipeline {
-  export const Repository = {
-    Region: 'ap-northeast-2',
-    Name: 'nextjs-example',
-  }
+export const Repository = {
+  Region: 'ap-northeast-2',
+  Name: 'nextjs-example',
 }
